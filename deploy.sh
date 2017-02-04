824b919307b5d571edb7a4cd8f878638cdc79e35
Rick van de Loo <rickvandeloo@gmail.com>
2017-02-04 18:18:15 +0100

    do not reinstall python3 and ansible in deploy
    --needed` -> `Do not reinstall the targets that are already up-to-date.`

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ fi;
 # Install bootstrap dependencies on Archlinux machines
 if type pacman > /dev/null 2>&1; then
     pacman -Syyu --noconfirm
-    pacman -S python3 ansible --noconfirm
+    pacman -S python3 ansible --noconfirm --needed
 fi;
 
 ansible-playbook provisioning/main.yml --connection=local --connection=local -i '127.0.0.1,' -vvv
